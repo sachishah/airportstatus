4f7afa8059af0aadaa10f76a03d61cc0ef874c49
Ari Lacenski <alacenski@gmail.com>
2013-09-27 02:11:47 -0700

    Dropping unused import

--- src/com/example/airportstatus/AirportStatusActivity.java
+++ src/com/example/airportstatus/AirportStatusActivity.java
@@ -18,7 +18,6 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.airportstatus.models.Favorite;
